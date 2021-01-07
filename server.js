6a1977a58a645f872ce034206ad0891d34cfa8b3
graceyraspberry <gracelam95122@gmail.com>
2021-01-07 00:50:07 -0800

    removing extra public files

--- server.js
+++ server.js
@@ -47,9 +47,9 @@ app.listen(port, () => {
 });
 
 // Serve static files from the React frontend app
-app.use(express.static(path.join(__dirname, "./client/public")));
+app.use(express.static(path.join(__dirname, "../client/public")));
 
 // AFTER defining routes: Anything that doesn't match what's above, send back index.html; (the beginning slash ('/') in the string is important!)
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname + "/./client/public/index.html"));
+  res.sendFile(path.join(__dirname + "/../client/public/index.html"));
 });
